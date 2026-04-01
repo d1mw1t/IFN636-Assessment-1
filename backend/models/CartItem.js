0bf3331171381a46403dee342aa223ec6a271e9a
d1mw1t <crydmwt@gmail.com>
2026-04-01 16:33:37 +1000

    Added comments to CartItem

--- backend/models/CartItem.js
+++ backend/models/CartItem.js
@@ -1,35 +1,40 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); //Import mongoose library
 
-const cartItemSchema = new mongoose.Schema(
+const cartItemSchema = new mongoose.Schema( //Blueprint
   {
+    //Name req
     name: {
       type: String,
       required: true,
       trim: true,
     },
+    //Category req
     category: {
       type: String,
       required: true,
       trim: true,
     },
+    //Price req
     price: {
       type: Number,
       required: true,
     },
+    //Image req
     image: {
       type: String,
       default: "",
     },
+    //Qty req
     quantity: {
       type: Number,
       required: true,
       default: 1,
-      min: 1,
+      min: 1, //Must be at least 1
     },
   },
   {
-    timestamps: true,
+    timestamps: true, //Allows for created and updated at
   },
 );
 
-module.exports = mongoose.model("CartItem", cartItemSchema);
+module.exports = mongoose.model("CartItem", cartItemSchema); //Creates a model. used by find()
