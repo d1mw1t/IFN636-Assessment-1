ee9b2e4a527f8831177ed172b2e52cd8c648b85f
d1mw1t <crydmwt@gmail.com>
2026-04-01 17:10:47 +1000

    Extended update and delete functions to cart context

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -1,16 +1,17 @@
 import { createContext, useState, useEffect } from "react";
 import axiosInstance from "../axiosConfig";
-//Creates a global state, stores data, keeps data on reload, handles API req
+// global cart state + api calls
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  //Provider of cart state to app
+  // wraps app
+
   const [cart, setCart] = useState([]);
-  //Store cart as an array
+  // cart array
 
   const fetchCart = async () => {
-    //READ
+    // READ
     try {
       const response = await axiosInstance.get("/api/cart");
       setCart(response.data);
@@ -20,7 +21,7 @@ export const CartProvider = ({ children }) => {
   };
 
   const addToCart = async (item) => {
-    //CREATE
+    // CREATE
     try {
       const response = await axiosInstance.post("/api/cart", item);
 
@@ -28,7 +29,7 @@ export const CartProvider = ({ children }) => {
         const existingItemIndex = prevCart.findIndex(
           (cartItem) => cartItem.name === response.data.name,
         );
-        //Avoids dupes
+        // check if already in cart
 
         if (existingItemIndex !== -1) {
           const updatedCart = [...prevCart];
@@ -43,13 +44,49 @@ export const CartProvider = ({ children }) => {
     }
   };
 
+  const updateCartItem = async (id, quantity) => {
+    // UPDATE
+    try {
+      const response = await axiosInstance.put(`/api/cart/${id}`, {
+        quantity,
+      });
+
+      setCart((prevCart) =>
+        prevCart.map((item) => (item._id === id ? response.data : item)),
+      );
+      // replace updated item
+    } catch (error) {
+      console.error("Error updating cart item:", error);
+    }
+  };
+
+  const deleteCartItem = async (id) => {
+    // DELETE
+    try {
+      await axiosInstance.delete(`/api/cart/${id}`);
+
+      setCart((prevCart) => prevCart.filter((item) => item._id !== id));
+      // remove from state
+    } catch (error) {
+      console.error("Error deleting cart item:", error);
+    }
+  };
+
   useEffect(() => {
     fetchCart();
   }, []);
-  //Runs on load
+  // run once on load
 
   return (
-    <CartContext.Provider value={{ cart, setCart, addToCart }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        setCart,
+        addToCart,
+        updateCartItem,
+        deleteCartItem,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
